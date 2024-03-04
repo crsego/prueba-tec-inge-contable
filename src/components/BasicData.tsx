f7198d6e5e3642ce809a42dab6f31b74cc6224fa
Cristian Serna <cristsergo@gmail.com>
2024-03-04 01:14:37 -0500

    improve styles

--- src/components/BasicData.tsx
+++ src/components/BasicData.tsx
@@ -17,39 +17,37 @@ function BasicData() {
     }
 
     return(
-        <div className="bg-dark bg-gradient d-flex flex-column w-75 h-75  align-self-center p-3 border rounded-4 shadow-lg">
-            <Form>
-                <Form.Group>
-                    <Form.Label>Nombre</Form.Label>
-                    <Form.Control type="text" placeholder="Juanito Perez" onChange={(e)=> setUName(e.currentTarget.value)}/>
-                </Form.Group>
-                <Form.Group className="d-flex flex-row">
-                    <Form.Label>Rol</Form.Label>
-                        {['Admin', 'User'].map((type)=>(
-                            <Form.Check 
-                            id="default-radio" 
-                            type="radio" 
-                            value={type} 
-                            className=""
-                            name="rol" 
-                            checked={uRol === type}
-                            onChange={(e)=> setURol(e.currentTarget.value)}
-                            >
-                                {type} 
-                            </Form.Check>
-                        ))}
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Especialidad</Form.Label>
-                    <Form.Select onChange={(e)=> setUSpecial(e.currentTarget.value)}>
-                        {['Seguridad', 'Ingeniero', 'Desarrollador'].map((type)=>(
-                            <option value={type}>{type}</option>
-                        ))}
-                    </Form.Select>
-                </Form.Group>
-                <Button type="button" onClick={() => updateAll()}>Guardar</Button>
-            </Form>
-        </div>
+        <Form className="bg-dark bg-gradient d-flex flex-column w-75 h-75  align-self-center p-3 rounded-4 shadow-lg text-light">
+            <Form.Group>
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type="text" placeholder="Juanito Perez" onChange={(e)=> setUName(e.currentTarget.value)}/>
+            </Form.Group>
+            <Form.Group className="d-flex flex-row my-3">
+                <Form.Label>Rol:</Form.Label>
+                    {['Admin', 'User'].map((type)=>(
+                        <Form.Check 
+                        id="default-radio" 
+                        className="mx-3 text-white-50"
+                        type="radio" 
+                        label={type}
+                        value={type} 
+                        checked={uRol === type}
+                        onChange={(e)=> setURol(e.currentTarget.value)}
+                        />
+                    ))}
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Especialidad</Form.Label>
+                <Form.Select onChange={(e)=> setUSpecial(e.currentTarget.value)}>
+                    {['Seguridad', 'Ingeniero', 'Desarrollador'].map((type)=>(
+                        <option value={type}>{type}</option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div className="d-flex justify-content-end mt-3">
+            <Button type="button" onClick={() => updateAll()}>Guardar</Button>
+            </div>
+        </Form>
     )
 }
 
